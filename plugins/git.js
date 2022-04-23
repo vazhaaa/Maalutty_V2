527caae88f72cd4dae9fdf2953e5d2e663821f37
vazhaaa <103118840+vazhaaa@users.noreply.github.com>
2022-04-23 18:41:11 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -19,27 +19,12 @@ Maalutty.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (mess
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `❤️Maalutty_v2💙
 
-*bot making video* : https://youtu.be/jF8wF1KdyOw
+VazhaMwonte Maalootty
 
 
-*owner number husni* :https://wa.me/917025868709.
-            
-
-*owner number Nijin* :https://wa.me/919605385305.
-
-
-*owner id instagram* :https://instagram.com/_husni_ser_.
-
-
-*github* : https://github.com/nijin-husni/Maalutty.
-
-
-*audio commands* :https://github.com/nijin-husni/uploads/tree/main/bgm.
-
-*sticker commands* :https://github.com/nijin-husni/uploads/tree/main/stickers.
 ■□■□■□■□■□■□■□■□■□■□
 ❤️Maalutty💙
-▣▣ ~NIJIN~ AND ~HUSNI~ ▣▣
+▣▣ ~VazhaMwon~ ▣▣
 
 `}) 
 
